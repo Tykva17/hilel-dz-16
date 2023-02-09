44bea3e9feadd951caa017b74954a84f23c10817
DimaTykva <dimatykvadanait@gmail.com>
2023-02-09 21:58:14 +0200

    dz 16

---
+++ script-dz-16.js
@@ -0,0 +1,15 @@
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateKey(length, characters){
+    let keyStr = '';
+    let charItem = characters.split('');
+    for(let i=0; i<= length; i++){
+        let item = Math.floor(Math.random() * characters.length);
+        keyStr += charItem[item]
+        console.log(item)
+    }
+    return keyStr;
+}
+
+const key = generateKey(16, characters);
+console.log(key); // eg599gb60q926j8i
